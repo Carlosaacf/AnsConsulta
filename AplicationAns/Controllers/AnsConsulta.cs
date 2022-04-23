8336f9f8e8297e79b2d2ee177e5a8ce208349094
carlosaacf <carlosaacf@gmail.com>
2022-04-23 06:03:49 -0300

    Adicionar arquivos de projeto.

---
+++ AplicationAns/Controllers/AnsConsulta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AplicationAns.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicationAns.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnsConsulta : ControllerBase
+    {
+        private static List<Cpf> Cpfs = new List<Cpf>();
+        public void AdicionarCpf(Cpf cpf)
+        {
+            Cpfs.Add(cpf);
+            Console.WriteLine(cpf.NomeTitular);
+
+        }
+    }
+}
---
+++ AplicationAns/Models/Cpf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicationAns.Models
+{
+    public class Cpf
+    {
+        public string NomeTitular { get; set; }
+
+        public int Prontuario { get; set; }
+
+        public string PlanoSaude { get; set; }
+
+
+
+    }
+}
